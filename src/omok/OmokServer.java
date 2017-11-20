5e60b5c28cf3e655d30ebce140b1eec6cbf3651c
Edwin-J <gkdhaosl@Naver.com>
2017-11-20 10:12:26 +0900

    Completed Server class

--- src/omok/OmokServer.java
+++ src/omok/OmokServer.java
@@ -5,11 +5,21 @@ import java.net.ServerSocket;
 
 public class OmokServer {
 
-	private ServerSocket socket;
-	private BManager manager = new BManager();
+	private ServerSocket serversocket;
+	private BManager bMan = new BManager();
 	
-	void startServer() throws Exception {
-		socket = new ServerSocker(7777);
+	void startServer() {
+		try {
+			serversocket = new ServerSocket(/*1234*/);
+			System.out.println("Created Server Socket : ");
+			Socket socket = server.accept();
+			OmokTread ot = new OmokThread(socket);
+			//ot.start();
+			//bMan.add(ot);
+			System.out.println("Connected : " + bMan.size());
+		} catch (Exeption e) {
+			System.out.println(e.toString);
+		}
 	}
 	
 	public static void main(String[] args) {
